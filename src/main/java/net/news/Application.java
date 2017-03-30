fbcb4b5643e0a06d84aefd09862dd6f559d50568
seko0716 <sergey3440@gmail.com>
2017-03-30 10:13:42 +0400

    @EnableAutoConfiguration

--- src/main/java/net/news/Application.java
+++ src/main/java/net/news/Application.java
@@ -2,6 +2,7 @@ package net.news;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
